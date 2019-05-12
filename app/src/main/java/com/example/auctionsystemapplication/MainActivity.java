2d0483f2329df0678483e4571a2ecfbb4a63757c
[anish rajbhandari] <[anishkalpakunj@gmail.com]>
2019-05-12 08:22:35 +0545

    Designed dashboard for admin

--- app/src/main/java/com/example/auctionsystemapplication/MainActivity.java
+++ app/src/main/java/com/example/auctionsystemapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.auctionsystemapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import adapter.MainActivityAdapter;
+import fragments.LogInFragment;
+import fragments.SignUpFragment;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabId);
+
+        MainActivityAdapter adapter = new MainActivityAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new LogInFragment(),"Log In");
+        adapter.addFragment(new SignUpFragment(),"Sign Up");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
--- app/src/main/java/fragments/LogInFragment.java
+++ app/src/main/java/fragments/LogInFragment.java
@@ -1,30 +1,39 @@
 package fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.auctionsystemapplication.AdminDashboardActivity;
 import com.example.auctionsystemapplication.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LogInFragment extends Fragment {
+public class LogInFragment extends Fragment implements View.OnClickListener{
+private AppCompatButton btnlogin;
 
 
-    public LogInFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_log_in, container, false);
+        btnlogin = view.findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getActivity(), AdminDashboardActivity.class);
+        startActivity(intent);
+    }
 }
